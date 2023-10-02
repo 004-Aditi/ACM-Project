ad2f9ad89bf589154c957f2a4573c6b1a440d3b9
004-Aditi <144404066+004-Aditi@users.noreply.github.com>
2023-10-02 06:32:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
